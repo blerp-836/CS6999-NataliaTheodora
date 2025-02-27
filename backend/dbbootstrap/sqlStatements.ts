eabbe5c8748c737767c863010ceac8ccb6bf2f19
andylulciuc <115028457+andylulciuc@users.noreply.github.com>
2025-02-27 12:05:38 -0500

    add read only user template and bootstrap script

--- backend/dbbootstrap/sqlStatements.ts
+++ backend/dbbootstrap/sqlStatements.ts
@@ -19,5 +19,10 @@ export const publishDbSqlStatements: Record<string, string> = {
   'Grant DML privs on public schema to user': 'GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA PUBLIC TO {{IamUser}}',
   'Grant usage on public schema to user': 'GRANT USAGE, CREATE ON SCHEMA PUBLIC TO {{IamUser}}',
   'Set default table privileges for user': 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO {{IamUser}}',
+
+  'Create ro user': `CREATE USER {{ReadOnlyUser}} with PASSWORD '{{ReadOnlyPass}}'`,
+  'Grant select privs on public schema to ro user': 'GRANT SELECT ON ALL TABLES IN SCHEMA PUBLIC TO {{ReadOnlyUser}}',
+  'Set default table privileges for ro user': 'ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO {{ReadOnlyUser}}',
+
   'Create pseudo anonymized events table': 'CREATE TABLE IF NOT EXISTS published_events (id bigint PRIMARY KEY, event_type VARCHAR(64), type_version VARCHAR(16), event JSONB NOT NULL, create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP)',
 };
